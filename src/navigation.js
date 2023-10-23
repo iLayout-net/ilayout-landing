0ef74adc183f9666fb67fb3f0c655e0d3d211ef3
vchitai <chitai.vct@gmail.com>
2023-10-23 22:39:49 +0700

    update layout

--- src/navigation.js
+++ src/navigation.js
@@ -90,6 +90,6 @@ export const footerData = {
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Copyright 2023 iLayout@Visual · All rights reserved.
+    Copyright 2023 iLayout@Visual. All rights reserved.
   `,
 };
